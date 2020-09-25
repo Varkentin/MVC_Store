991a4b08fef3b8c0ecfb42261465fe0b87ea6b48
Vark <wardi@mail.ru>
2020-09-25 21:08:47 +0700

    Дорабатываем административную зону, создаём таблицу в базе данных, добавляем модели представлений и контроллеры

--- Areas/Admin/Controllers/PagesController.cs
+++ Areas/Admin/Controllers/PagesController.cs
@@ -6,9 +6,9 @@ using System.Web.Mvc;
 
 namespace MVC_Store.Areas.Admin.Controllers
 {
-    public class DashBoardController : Controller
+    public class PagesController : Controller
     {
-
+        // GET: Admin/Pages
         public ActionResult Index()
         {
             return View();
