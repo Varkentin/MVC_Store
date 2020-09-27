73b48f1fd09ebcf3b3114e1ac321c317a9d64b2b
Vark <wardi@mail.ru>
2020-09-27 20:38:38 +0700

    добавил страницу создания товаров

--- Areas/Admin/Controllers/PagesController.cs
+++ Areas/Admin/Controllers/PagesController.cs
@@ -31,15 +31,61 @@ namespace MVC_Store.Areas.Admin.Controllers
         [HttpPost]
         public ActionResult AddPage(PageVM model)
         {
-           // проверка на валидность
-           // объявление переменных для краткого описания slug
-           //Иниц класс PageDTO
-           //Присвоить заголовок модели
-           //если !краткое описание то присвоить
-           // убеждаемся, что заголовок и краткое описание уникальны
-           // присваиваем ост  значения модели
-           // сохраняем модель в базу данных
-           // передаем сообщения через TempData;
+            // проверка на валидность
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            using (Db db = new Db())
+            {
+                // объявление переменных для краткого описания slug
+                string slug;
+               
+                //Иниц класс PageDTO
+                PagesDTO dto = new PagesDTO();
+
+                //Присвоить заголовок модели
+                dto.Title = model.Title.ToUpper();
+               
+                //если !краткое описание то присвоить
+                if(string.IsNullOrWhiteSpace(model.Slug))
+                {
+                    slug = model.Title.Replace(" ", "-").ToLower();
+
+                }
+                else
+                {
+                    slug = model.Slug.Replace(" ", "-").ToLower();
+                }
+                
+                // убеждаемся, что заголовок и краткое описание уникальны
+                if(db.Pages.Any(x => x.Title == model.Title))
+                {
+                    ModelState.AddModelError("", "That title allready exist.");
+                        return View(model);
+                }
+                else if(db.Pages.Any(x => x.Slug == model.Slug))
+                {
+                    ModelState.AddModelError("", "That slug allready exist.");
+                    return View(model);
+                }
+               
+                // присваиваем ост  значения модели
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSideBar = model.HasSideBar;
+                dto.Sorting = 100;
+
+                // сохраняем модель в базу данных
+                db.Pages.Add(dto);
+                db.SaveChanges();
+            }
+            // передаем сообщения через TempData;
+            TempData["SM"] = "You have added a new page";
+            
+            // преадрессовываем пользователя на метод индекс
+            return RedirectToAction("Index");
+
         }
     }
 
--- Models/ViewModels/Pages/PageVM.cs
+++ Models/ViewModels/Pages/PageVM.cs
@@ -22,13 +22,15 @@ namespace MVC_Store.Models.ViewModels.Pages
         }
         public int Id { get; set; }
         [Required]
-        [StringLength(50,MinimumLength = 3)]
+        [StringLength(50, MinimumLength = 3)]
         public string Title { get; set; }
         public string Slug { get; set; }
         [Required]
         [StringLength(int.MaxValue, MinimumLength = 3)]
         public string Body { get; set; }
         public int Sorting { get; set; }
+        
+        [Display(Name ="SideBar")]
         public bool HasSideBar { get; set; }
     }
 }
